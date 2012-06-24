b2cacaeff28e9f8c8708ae20c9f72099275d134b
Daryl Antony <daryl@commoncode.com.au>
2012-06-24 15:52:42 +1000

    setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup( name='infrastruction',
+    version = '0.0.1',
+    description = 'Infrastructure bootstrapping for Modern Django applications',
+    author = 'Daryl Antony',
+    author_email = 'daryl@commoncode.com.au',
+    url = 'https://github.com/commoncode/infrastruction',
+    keywords = ['django',],
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires = [
+        'Django>=1.4',
+    ]
+)
