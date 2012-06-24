58a931ad65e18314c4f1cf602cbcd0a60fd34413
Daryl Antony <daryl@commoncode.com.au>
2012-06-24 18:38:03 +1000

    base urls

---
+++ infrastruction/urls.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from filebrowser.sites import site
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/filebrowser/', include(site.urls)),
+    url(r'^grappelli/', include('grappelli.urls')),
+
+)
+
+#
+# DEBUG MEDIA SETTINGS - Django Serves Static for us, but we must serve Media
+#
+
+if hasattr(settings, 'SERVE_MEDIA') and settings.SERVE_MEDIA:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += patterns('django.views.static',
+        url(r'^media/(?P<path>.*)$',
+            'serve',
+            { 'document_root': settings.MEDIA_ROOT, },
+        ),
+    ) + staticfiles_urlpatterns()
